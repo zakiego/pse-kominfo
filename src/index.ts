078d374206a0e3adc85171e0f5f26cd1d9c91092
zakiego <belummikir@gmail.com>
2022-07-30 17:03:21 +0800

    feat: test try cath

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,12 @@ import {
   fetchApi,
   combineFile,
   endpointList,
+  consoleTitle,
 } from "./utils";
 
 async function launch(nameFile: string, url: string) {
   try {
-    console.log(
-      `\n\n===================== ${nameFile} =====================\n`,
-    );
+    consoleTitle(nameFile);
 
     const lastPage = (await getLastPage(url)) - 1;
 
@@ -33,12 +32,12 @@ async function launch(nameFile: string, url: string) {
 async function main() {
   const api = endpointList();
 
-  await launch("lokal-terdaftar", api.lokal.terdaftar);
-  await launch("lokal-dicabut", api.lokal.dicabut);
-  await launch("lokal-dihentikan-sementara", api.lokal.dihentikanSementara);
-  await launch("asing-terdaftar", api.asing.terdaftar);
-  await launch("asing-dicabut", api.asing.dicabut);
-  await launch("asing-dihentikan-sementara", api.asing.dihentikanSementara);
+  // await launch("lokal-terdaftar", api.lokal.terdaftar);
+  // await launch("lokal-dicabut", api.lokal.dicabut);
+  // await launch("lokal-dihentikan-sementara", api.lokal.dihentikanSementara);
+  // await launch("asing-terdaftar", api.asing.terdaftar);
+  // await launch("asing-dicabut", api.asing.dicabut);
+  // await launch("asing-dihentikan-sementara", api.asing.dihentikanSementara);
 
   combineFile();
   createMaster();
