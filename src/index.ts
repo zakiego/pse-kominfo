851c77ff6acbaf60e744fafcf0526086dd3ce69b
zakiego <belummikir@gmail.com>
2022-07-30 19:15:54 +0800

    feat: zero tolerance error fetch last page

--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,32 @@ import {
 } from "./utils";
 
 async function launch(nameFile: string, url: string) {
+  consoleTitle(nameFile);
+
+  let lastPage: number;
+
   try {
-    consoleTitle(nameFile);
-
-    const lastPage = (await getLastPage(url)) - 1;
-
-    const bucket = [];
-
-    for (let i = 0; i <= lastPage; i++) {
-      const data = await fetchApi(url, i);
-      console.log(
-        `Fetch data ${nameFile} - halaman ${data.raw.meta.page.currentPage}`,
-      );
-      bucket.push(...data.clean);
-
-      saveJSON(`data/json-raw/${nameFile}/${i}.json`, data.raw);
-    }
-
-    saveJSON(`data/json/${nameFile}.json`, bucket);
-    saveCSV(`data/csv/${nameFile}.csv`, bucket);
-  } catch (error) {
-    console.log(error);
+    lastPage = (await getLastPage(url)) - 1;
+  } catch (err) {
+    // ketika tidak ada last page, berarti return bukan json, skip
+    console.log(err);
+    return;
   }
+
+  const bucket = [];
+
+  for (let i = 0; i <= lastPage; i++) {
+    const data = await fetchApi(url, i);
+    console.log(
+      `Fetch data ${nameFile} - halaman ${data.raw.meta.page.currentPage}`,
+    );
+    bucket.push(...data.clean);
+
+    saveJSON(`data/json-raw/${nameFile}/${i}.json`, data.raw);
+  }
+
+  saveJSON(`data/json/${nameFile}.json`, bucket);
+  saveCSV(`data/csv/${nameFile}.csv`, bucket);
 }
 
 async function main() {
@@ -42,6 +46,7 @@ async function main() {
   await launch("lokal-terdaftar", api.lokal.terdaftar);
   await launch("lokal-dicabut", api.lokal.dicabut);
   await launch("lokal-dihentikan-sementara", api.lokal.dihentikanSementara);
+
   await launch("asing-terdaftar", api.asing.terdaftar);
   await launch("asing-dicabut", api.asing.dicabut);
   await launch("asing-dihentikan-sementara", api.asing.dihentikanSementara);
