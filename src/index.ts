bfa5f17b0b0042d9bcf4191dc4caefea64789fbb
zakiego <belummikir@gmail.com>
2022-07-30 06:32:44 +0000

    mock

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,57 @@
 import {
   createMaster,
   endpointList,
-  getData,
+  getLastPage,
   saveCSV,
   saveJSON,
+  fetchApi,
 } from "./utils";
 
-async function launch(nameFile: string, url: string) {
-  console.log("Trying to get data from", url);
-  const data = await getData(url);
+import Queue from "js-queue";
 
-  saveJSON(`data/json-raw/${nameFile}.json`, data.raw);
-  saveJSON(`data/json/${nameFile}.json`, data.clean);
-  saveCSV(`data/csv/${nameFile}.csv`, data.clean);
+async function launch(nameFile: string, url: string) {
+  const queue = new Queue();
+
+  // console.log("Trying to get data from", url);
+
+  const lastPage = (await getLastPage(url)) - 1;
+
+  let x = 0;
+
+  for (let i = 0; i <= lastPage; i++) {
+    queue.add(iterateData);
+  }
+
+  async function iterateData() {
+    const data = await fetchApi(url, x);
+    x = x + 1;
+    console.log(data.raw.meta.page.currentPage);
+    saveJSON(`data/json-raw/${nameFile}/${x}.json`, data.raw);
+    saveJSON(`data/json/${nameFile}/${x}.json`, data.clean);
+    saveCSV(`data/csv/${nameFile}/${x}.csv`, data.clean);
+
+    this.next();
+  }
+
+  // const data = await getData(url);
+
+  // saveJSON(`data/json-raw/${nameFile}.json`, data.raw);
+  // saveJSON(`data/json/${nameFile}.json`, data.clean);
+  // saveCSV(`data/csv/${nameFile}.csv`, data.clean);
 }
 
 async function main() {
   const api = endpointList();
 
   await launch("lokal-terdaftar", api.lokal.terdaftar);
-  await launch("lokal-dicabut", api.lokal.dicabut);
-  await launch("lokal-dihentikan-sementara", api.lokal.dihentikanSementara);
+  // await launch("lokal-dicabut", api.lokal.dicabut);
+  // await launch("lokal-dihentikan-sementara", api.lokal.dihentikanSementara);
 
-  await launch("asing-terdaftar", api.asing.terdaftar);
-  await launch("asing-dicabut", api.asing.dicabut);
-  await launch("asing-dihentikan-sementara", api.asing.dihentikanSementara);
+  // await launch("asing-terdaftar", api.asing.terdaftar);
+  // await launch("asing-dicabut", api.asing.dicabut);
+  // await launch("asing-dihentikan-sementara", api.asing.dihentikanSementara);
 
-  createMaster();
+  // createMaster();
 }
 
 main();
--- src/utils.ts
+++ src/utils.ts
@@ -3,31 +3,35 @@ import { Endpoint, Resp } from "./types";
 import converter from "json-2-csv";
 import path from "path";
 import { flattenDeep, flatten } from "lodash";
+import { fetch } from "node-fetch-native";
 
 export function endpointList() {
   const file = fs.readFileSync("api.json", "utf8");
   return JSON.parse(file) as Endpoint;
 }
 
-export async function fetchApi(url: string) {
-  const updateUrl = new URL(url);
-  updateUrl.searchParams.set("page[limit]", "100000");
+export async function getLastPage(url: string) {
+  const data: Resp = await fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    console.log("Error:", response.status);
+    throw new Error("Something went wrong");
+  });
 
-  const data = await fetch(updateUrl.toString())
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      console.log("Error:", response.status);
-      throw new Error("Something went wrong");
-    })
-    .then((resp) => resp.json());
-
-  return data as Resp;
+  return data.meta.page.lastPage;
 }
 
-export async function getData(url: string) {
-  const raw = await fetchApi(url);
+export async function fetchApi(url: string, index: number) {
+  const updateUrl = url.replace("0.json", `${index}.json`);
+
+  const raw: Resp = await fetch(updateUrl.toString()).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    console.log("Error:", response.status);
+    throw new Error("Something went wrong");
+  });
 
   const clean = raw.data.map((item) => {
     const { type, id, attributes, links } = item;
@@ -36,6 +40,17 @@ export async function getData(url: string) {
 
   return { raw, clean };
 }
+
+// export async function getData(url: string) {
+//   const raw = await fetchApi(url);
+
+//   const clean = raw.data.map((item) => {
+//     const { type, id, attributes, links } = item;
+//     return { type, id, ...attributes, ...links };
+//   });
+
+//   return { raw, clean };
+// }
 
 export async function saveJSON(path: string, data: any) {
   fs.writeFileSync(path, JSON.stringify(data, null, 2));
